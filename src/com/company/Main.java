97685bfb27d04b21911b9e3ce5ebddc7d5426509
MuhammadAbyaz <abyazkhalid2005@gmail.com>
2021-04-27 15:34:54 +0500

    Made code clear

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
+        //Constants
         final int PERCENT =100;
         final int MONTHS = 12;
+
         Scanner sc = new Scanner(System.in);
+
         //Loan
         System.out.print("Principal($): ");
         int principal = sc.nextInt();
+
         // monthly interest
         System.out.print("Monthly interest rate: ");
-        double annualInterestRate = (sc.nextDouble() / PERCENT);
+        double annualInterestRate = sc.nextDouble();
+        double monthlyInterestRate = annualInterestRate/ PERCENT;
+
+        //Period
         System.out.print("Period (Years): ");
-        int period = sc.nextInt()*MONTHS;
-        double numerator = annualInterestRate*(Math.pow((1 + annualInterestRate), (int)period));
-        double denominator = (Math.pow((1+ annualInterestRate), (int)period)) -1;
+        byte period = sc.nextByte();
+        int numberOfPayments = period*MONTHS;
+
+        //numerator
+        double numerator = monthlyInterestRate*(Math.pow((1 + monthlyInterestRate), (int)numberOfPayments));
+
+        //denominator
+        double denominator = (Math.pow((1+ monthlyInterestRate), (int)numberOfPayments)) -1;
+
+        //Mortgage
         double mortgage = (int)principal * (numerator / denominator);
         NumberFormat dollar = NumberFormat.getCurrencyInstance();
         String mortgageResult = dollar.format(mortgage);
